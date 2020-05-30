e45ac0c83acd5c1465d8c5e36d7964072b76b2b7
IndrajitFED <36766296+IndrajitFED@users.noreply.github.com>
2020-05-30 17:42:38 +0530

    Update Balancing_Stars.cpp

--- Balancing_Stars.cpp
+++ Balancing_Stars.cpp
@@ -17,33 +17,30 @@ int main() {
     }
 
     int len = str.length();
-    int cor_brac = 0;
+    int valid_bra = 0;
     int corr_cnt =0;
-    for(int i=0;i<len;i++){
-        if(!stk.empty()){
-            pair<char,int> tp = stk.top();
-            if(tp.first == '(' && str[i] == ')' || tp.first == '[' && str[i] == ']' || 
-            tp.first == '{' && str[i] == '}' ){
-                cor_brac++;
-                stk.pop();
-                int l = tp.second + 1;
-                int r = i - 1;
-                int k = 0;
-                while(l < r){
-                    if(str[l] != '*' || str[r] != '*') break;
-                    if(str[l] == '*') l++, k++;
-                    if(str[r] == '*') r--, k++;
+    
+    for(int i=0;i< len;i++) {
+            if(!stk.empty()) {
+                pair<char, int> tp = stk.top();
+                if((tp.first=='(' && str[i]==')') || (tp.first=='[' && str[i]==']') || (tp.first=='{' && str[i]=='}')) {
+                    valid_bra++;
+                    stk.pop();
+                    // check for stars
+                    int l = tp.second+1, r = i-1, k=0;
+                    while(l<r) {
+                        if(str[l]!='*' && str[r]!='*') break;
+                        if(str[l]=='*') k++, l++;
+                        if(str[r]=='*') k++, r--;
+                    }
+                    if(k>=2) corr_cnt++;
                 }
-                if(k >= 2){
-                    corr_cnt++;
-                }
+                else if(str[i]!='*') stk.push({str[i], i});
             }
-            else if(str[i] != '*') stk.push({str[i],i});
+            else if(str[i]!='*') stk.push({str[i], i});
         }
-        else if(str[i] != '*') stk.push({str[i],i});
-    }
-    if(stk.empty() && corr_cnt == cor_brac) cout << "YES " << cor_brac <<endl;
-    else cout << "NO " << cor_brac <<endl;
+
+    if(stk.empty() && corr_cnt == valid_bra) cout << "YES " << corr_cnt <<endl;
+    else cout << "NO " << corr_cnt <<endl;
     return 0;
 }
-
